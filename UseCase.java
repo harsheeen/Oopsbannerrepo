4c3c040131c1254a079e6604efbbdf66ba8e135c
harsheeen <harsheenkaur023@gmail.com>
2026-02-18 14:52:26 +0530

    usecaseone

---
+++ UseCase.java
@@ -0,0 +1,12 @@
+/** 
+* UC1 BANNER - printing "oops banner" 
+* @author developer
+*@version 1
+**/
+
+
+class UseCase{
+public static void main(String args[]){
+System.out.println("OOPS BANNER");
+}
+}
