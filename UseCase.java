d51d60a35143f7ba56daa5c93419b525f8488db8
harsheeen <harsheenkaur023@gmail.com>
2026-02-18 16:10:40 +0530

    fourthcommit

--- UseCase.java
+++ UseCase.java
@@ -1,21 +1,23 @@
-
-
 /** 
-* OOPSBANNERAPP UC3 - OOPS BANNER Display Application with join function
-* @author Developer
-* @version 3.0
+* OOPSBANNERAPP UC4 - OOPS BANNER Display Application with array and for each
+* @author Harsheen
+* @version 4.0
 **/
 class UseCase{
 	public static void main(String[] args){
-		System.out.println(String.join(" 2 " ,"   ***   "  ,  "   ***   "  ,  " ******   " ,  "  ***** "));
-		System.out.println(String.join(" 2 " ," **   **"  ,  "  **   **"  ,  "  **   **"  ,   " **"));
-		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " **    **"  ,  "**"));
-		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  , "  **   **"  ,    " **"));
-		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " ****** "  ,  "   ****"));
-		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "      **"));
-		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "       **"));
-		System.out.println(String.join(" 2 " ," **   **"  ,  "  **   **"  ,  "  **     "  ,  "      **"));
-		System.out.println(String.join(" 2 " ,"   ***    " ,"  ***  "  ,"  **  "  ,   "    ***** "));
-
+		String[] array=new String[9];
+		array[0]=String.join(" " ,"   ***   "  ,  "   ***   "  ,  " ******   " ,  "  ***** ");
+		array[1]=String.join(" " ," **   **"  ,  "  **   **"  ,  "  **   **"  ,   " **");
+		array[2]=String.join(" " ,"**     **"  ,  "**     **"  ,  " **    **"  ,  "**");
+		array[3]=String.join(" " ,"**     **"  ,  "**     **"  , "  **   **"  ,    " **");
+		array[4]=String.join(" " ,"**     **"  ,  "**     **"  ,  " ****** "  ,  "   ****");
+		array[5]=String.join(" " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "      **");
+		array[6]=String.join(" " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "       **");
+		array[7]=String.join(" " ," **   **"  ,  "  **   **"  ,  "  **     "  ,  "      **");
+		array[8]=String.join(" " ,"   ***    " ,"  ***  "  ,"  **  "  ,   "    ***** ");
+		for(String len : array)
+			System.out.println(len);
+	
+	
 	}
 }
