ffb3170241aa1daa30b3041fe7cb7dff605eb188
harsheeen <harsheenkaur023@gmail.com>
2026-02-18 15:50:46 +0530

    thirdcase

---
+++ UseCase.java
@@ -0,0 +1,19 @@
+
+/** 
+* OOPSBANNERAPP UC3 - OOPS BANNER Display Application with join function
+* @author Developer
+* @version 3.0
+**/
+class UseCase{
+	public static void main(String[] args){
+		System.out.println(String.join(" 2 " ,"   ***   "  ,  "   ***   "  ,  " ******   " ,  "  ***** "));
+		System.out.println(String.join(" 2 " ," **   **"  ,  "  **   **"  ,  "  **   **"  ,   " **"));
+		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " **    **"  ,  "**"));
+		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  , "  **   **"  ,    " **"));
+		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " ****** "  ,  "   ****"));
+		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "      **"));
+		System.out.println(String.join(" 2 " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "       **"));
+		System.out.println(String.join(" 2 " ," **   **"  ,  "  **   **"  ,  "  **     "  ,  "      **"));
+		System.out.println(String.join(" 2 " ,"   ***    " ,"  ***  "  ,"  **  "  ,   "    ***** "));
+	}
+}
