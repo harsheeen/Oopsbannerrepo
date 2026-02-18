40b05a3cf23fc8d8bb9d994dd584507959e35c76
harsheeen <harsheenkaur023@gmail.com>
2026-02-18 16:25:46 +0530

    fifthcommit

--- UseCase.java
+++ UseCase.java
@@ -1,22 +1,24 @@
 /** 
-* OOPSBANNERAPP UC4 - OOPS BANNER Display Application with array and for each
+* OOPSBANNERAPP UC5 - OOPS BANNER Display Application with array without new key word and for each loop
 * @author Harsheen
-* @version 4.0
+* @version 5.0
 **/
 class UseCase{
 	public static void main(String[] args){
-		String[] array=new String[9];
-		array[0]=String.join(" " ,"   ***   "  ,  "   ***   "  ,  " ******   " ,  "  ***** ");
-		array[1]=String.join(" " ," **   **"  ,  "  **   **"  ,  "  **   **"  ,   " **");
-		array[2]=String.join(" " ,"**     **"  ,  "**     **"  ,  " **    **"  ,  "**");
-		array[3]=String.join(" " ,"**     **"  ,  "**     **"  , "  **   **"  ,    " **");
-		array[4]=String.join(" " ,"**     **"  ,  "**     **"  ,  " ****** "  ,  "   ****");
-		array[5]=String.join(" " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "      **");
-		array[6]=String.join(" " ,"**     **"  ,  "**     **"  ,  " **     "  ,  "       **");
-		array[7]=String.join(" " ," **   **"  ,  "  **   **"  ,  "  **     "  ,  "      **");
-		array[8]=String.join(" " ,"   ***    " ,"  ***  "  ,"  **  "  ,   "    ***** ");
-		for(String len : array)
-			System.out.println(len);
+	String array[]={
+		 "   ***   " + " " + "   ***   " + " " + " ******   " + "  ***** ",
+		 " **   **" + " " + "  **   **" + " " + "  **   **" + " " +  " **",
+		 "**     **" + " " + "**     **" + " " + " **    **" + " " + "**",
+		 "**     **" + " " + "**     **" + "  **   **" + " " + " " + " **",
+		 "**     **" + " " + "**     **" + " " + " ****** " + " " + "   ****",
+		 "**     **" + " " + "**     **" + " " + " **     " + " " + "      **",
+		 "**     **" + " " + "**     **" + " " + " **     " + " " + "       **",
+		 " **   **" + " " + "  **   **" + " " + "  **     " + " " + "      **",
+		 "   ***   " + " " + "   ***   " + " " + " **     " + "  ***** "};
+		 
+		 for(String name:array){
+		 System.out.println(name);
+		 }
 	
 	
 	}
