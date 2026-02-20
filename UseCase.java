d3f267fc0932da5b9057fcf7416f8a6430648955
harsheeen <harsheenkaur023@gmail.com>
2026-02-20 11:24:42 +0530

    eight commit

--- UseCase.java
+++ UseCase.java
@@ -1,35 +1,12 @@
-/**
- * OOPS banner app UC7 - Printing with inner and outer class concept
- * @author Harsheen
- * @version 7.0
- */
-
-public class UseCase{
-
-    // Inner static class 
-		static class CharacterPatternMap {
-        private char ch;
-        private String[] pattern;
-
-        // Constructor
-        public CharacterPatternMap(char ch, String[] pattern) {
-            this.ch = ch;
-            this.pattern = pattern;
-        }
-
-        // Getter - pattern
-        public String[] getPattern() {
-            return pattern;
-        }
-
-        // Getter - character
-        public char getChar() {
-            return ch;
-        }
-    }
-
-    public static CharacterPatternMap[] createCharacterPatternMaps() {
-        String[] O = { 
+/** use case 8
+@author Harsheen 
+@version 8
+**/
+import java.util.*;
+class UseCase{
+	public static HashMap<Character,String[]> createhashmap(){
+		HashMap<Character,String[]> charmap=new HashMap<>();
+		charmap.put('o',new String[] { 
             "   ***   ",
             " **   **",
             "**     **",
@@ -39,10 +16,8 @@ public class UseCase{
             "**     **",
             " **   **",
             "   ***   " 
-        };
-
-        String[] P = {
-            " ******   ",
+        });
+		charmap.put('p', new String[] { " ******   ",
             " **    **",
             " **     **",
             " **    **",
@@ -51,9 +26,9 @@ public class UseCase{
             " **     ",
             " **     ",
             " **   "
-        };
-
-        String[] S = {
+        });
+		
+		charmap.put('s', new String[]  {
             " ******     ",
             " **         ",
             " **         ",
@@ -63,42 +38,31 @@ public class UseCase{
             "     **     ",
             "     **     ",
             " ******   "
-        };
+        });
+		return charmap;
+		
+}
+public static void displaybanner(String message, HashMap<Character,String[]> charMap){
+	int patternHeight=charMap.get('o').length;
+	
+	char[] messages=message.toCharArray();
+	for(int line =0;line<patternHeight;line++){
+		StringBuilder str=new StringBuilder();
+	for(int i=0;i<messages.length;i++){
+		String[] temp=charMap.get(message.charAt(i));
+		str.append(temp[line]);
+	}
+			System.out.println(str);
 
-        CharacterPatternMap o = new CharacterPatternMap('O', O);
-        CharacterPatternMap p = new CharacterPatternMap('P', P);
-        CharacterPatternMap s = new CharacterPatternMap('S', S);
+}
 
-        return new CharacterPatternMap[]{o, p, s};
-    }
+	}
 
-    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMaps) {
-        for (CharacterPatternMap map : charMaps) {
-            if (map.getChar() == ch) {
-            return map.getPattern();
-            }
-        }
-    return null; // if character not found
-    }
-
-    public static void printMessage(String message, CharacterPatternMap[] charMaps) {
-        // Assume all patterns have the same number of rows
-        int rows = charMaps[0].getPattern().length;
-
-    for (int row = 0; row < rows; row++) {
-    StringBuilder line = new StringBuilder();
-         for (char ch : message.toCharArray()) {
-         String[] pattern = getCharacterPattern(ch, charMaps);
-          if (pattern != null) {
-           line.append(pattern[row]).append("  ");
-                }
-            }
-    System.out.println(line.toString());
-        }
-    }
-
-    public static void main(String[] args) {
-        CharacterPatternMap[] charMaps = createCharacterPatternMaps();
-        printMessage("OOPS", charMaps);
-    }
+public static void main(String args[]){
+	HashMap<Character,String[]> hashmaps=new HashMap<>();
+	hashmaps=createhashmap();
+	displaybanner("oops", hashmaps);
+	
+	
+}
 }
